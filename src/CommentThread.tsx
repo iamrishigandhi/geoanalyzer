aa9f8fc237ba72a192fa9d62db250e6f1801e794
Rishi Gandhi <rishigandhi2002@gmail.com>
2026-03-12 11:10:31 -0230

    fix build errors

--- src/CommentThread.tsx
+++ src/CommentThread.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import * as THREE from 'three';
 
 export default function CommentThread({
     comment,
--- src/MeasurementTools.tsx
+++ src/MeasurementTools.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface MeasurementToolsProps {
     activeTool: 'none' | 'distance' | 'angle' | 'comment';
     onToolChange: (tool: 'none' | 'distance' | 'angle' | 'comment') => void;
